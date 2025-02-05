7fa7206be5eb2c4a6ed89f436a3449769b7a799e
Nelsin Martin-Burnett <73394875+NelsinMB@users.noreply.github.com>
2025-02-05 18:20:23 -0500

    Updates Navbar design, Projects page, and color.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,33 @@
-// components/Navbar.js
+
 import Link from 'next/link';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
-        <li>
-          <Link href="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href="/projects">
-            Projects
-          </Link>
-        </li>
-      </ul>
+      <div className="nav-container">
+        <ul>
+          <li>
+            <Link href="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/writing">
+              Writing
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link href="/speaking">
+              Speaking
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
