33d1952f24a0ec3193a3885770334bbe16a2d550
Nelsin Martin-Burnett <73394875+NelsinMB@users.noreply.github.com>
2025-08-11 16:22:24 +0200

    Adds an about page

--- app/about/page.js
+++ app/about/page.js
@@ -5,8 +5,26 @@ import styles from "./About.module.css";
 export default function AboutPage() {
 
   return (
-    <div className="container">
+    <main className={styles.page}>
+        <div className={styles.text}>
+          <h1 className={styles.name}>Nelsin Martin-Burnett</h1>
+          <p>
+            I'm a recent graduate from McGill University, where I completed the Joint-Honours Maths & CS program.
+          </p>
+          <p>
+            I’m originally from <strong>Singapore</strong> and currently live in <strong>Barcelona</strong>.
+          </p>
 
-     </div>
+          <p>From time to time, I will work on this website.</p>
+
+          <p>I hope to have a blog on it, as well as a place to post photos from my trips.</p>
+
+          <div>
+            <a href="https://www.linkedin.com/in/nelsin-martin-burnett-5017461b4/">[LinkedIn] </a>
+            <a href="mailto:nelsinmburnett@gmail.com">[Email] </a>
+            <a href="https://www.github.com/nelsinmb">[Github]</a>
+          </div>
+      </div>
+    </main>
   );
 }
