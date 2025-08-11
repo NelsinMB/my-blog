80507e3c2d24b0cfb057146dd3da93aa71c58f40
Nelsin Martin-Burnett <73394875+NelsinMB@users.noreply.github.com>
2025-08-11 16:21:25 +0200

    Stops the Navbar and Footer fromb being displayed

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
